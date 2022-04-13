71888f4425a6d780ce258189e909bd5b86766f2b
Thinh Nguyen <gaupoit@outlook.com>
2022-04-13 10:28:23 +0700

    Change shell shape

--- src/js/index/SkyOctahedronShell.js
+++ src/js/index/SkyOctahedronShell.js
@@ -1,28 +1,28 @@
-const THREE = require('three');
+const THREE = require("three");
 
 export default class SkyOctahedronShell {
   constructor() {
     this.uniforms = {
       time: {
-        type: 'f',
-        value: 0
+        type: "f",
+        value: 0,
       },
     };
     this.obj = this.createObj();
   }
   createObj() {
-    const geometry = new THREE.OctahedronGeometry(150, 20);
+    const geometry = new THREE.BoxGeometry(160, 160, 160);
     return new THREE.Mesh(
       geometry,
       new THREE.RawShaderMaterial({
         uniforms: this.uniforms,
-        vertexShader: require('./glsl/skyOctahedronShell.vs').default,
-        fragmentShader: require('./glsl/skyOctahedronShell.fs').default,
+        vertexShader: require("./glsl/skyOctahedronShell.vs").default,
+        fragmentShader: require("./glsl/skyOctahedronShell.fs").default,
         transparent: true,
         side: THREE.DoubleSide,
-        depthWrite: false
+        depthWrite: false,
       })
-    )
+    );
   }
   render(time) {
     this.uniforms.time.value += time;
